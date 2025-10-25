e7ca7c4643cef4268d035092cf56080c81dd8ef0
dahiann04 <20243ds121@utez.edu.mx>
2025-10-24 18:10:07 -0600

    primera fase del diseño del registro

--- app/src/main/java/com/example/dely/ui/screens/RegistroScreen.kt
+++ app/src/main/java/com/example/dely/ui/screens/RegistroScreen.kt
@@ -1,5 +1,7 @@
 package com.example.dely.ui.screens
 
+import LoginScreen
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,9 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.dely.ui.theme.DelyTheme
+import com.example.dely.ui.viewmodel.LoginViewModel
 import com.example.dely.ui.viewmodel.MenuViewModel
+import com.example.dely.ui.viewmodel.RegistroViewModel
 
 
 @Composable
@@ -29,5 +36,18 @@ fun RegistroScreen(navController: NavController) {
                 text = "Registro"
             )
         }
+    }
+}
+
+@SuppressLint("ViewModelConstructorInComposable")
+@Preview(showBackground = true)
+@Composable
+fun PreviewRegistro() {
+    DelyTheme {
+        val navController = rememberNavController()
+
+        RegistroScreen(
+            navController = navController
+        )
     }
 }
