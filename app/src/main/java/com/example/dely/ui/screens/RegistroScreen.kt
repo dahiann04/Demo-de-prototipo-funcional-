cee753ef4be7a9dd3172e6308c737581e1b732cf
dahiann04 <20243ds121@utez.edu.mx>
2025-10-25 13:36:20 -0600

    Agrege encabeado de registro

--- app/src/main/java/com/example/dely/ui/screens/RegistroScreen.kt
+++ app/src/main/java/com/example/dely/ui/screens/RegistroScreen.kt
@@ -2,42 +2,92 @@ package com.example.dely.ui.screens
 
 import LoginScreen
 import android.annotation.SuppressLint
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.dely.ui.buttons.InputText
 import com.example.dely.ui.theme.DelyTheme
 import com.example.dely.ui.viewmodel.LoginViewModel
 import com.example.dely.ui.viewmodel.MenuViewModel
 import com.example.dely.ui.viewmodel.RegistroViewModel
-
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.dely.ui.buttons.PrimaryButton
 
 @Composable
 fun RegistroScreen(navController: NavController) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(20.dp),
-        modifier = Modifier.padding(30.dp)
+        modifier = Modifier.padding(25.dp)
     ) {
-        Row(modifier = Modifier.fillMaxWidth()) {
-        }
-        Row(modifier = Modifier.fillMaxWidth()) {
+
             Text(
-                text = "Registro"
+                text = "Registro",
+                fontWeight = FontWeight.Bold,
+                fontSize = 36.sp,
+                fontFamily = FontFamily.Cursive,
             )
+        val viewModel: RegistroViewModel = viewModel()
+        InputText(
+            value = viewModel.username.value,
+            onValueChange = { viewModel.username.value = it },
+            text = "Usuario",
+        )
+        InputText(
+            value = viewModel.email.value,
+            onValueChange = { viewModel.email.value = it },
+            text = "Correo",
+        )
+        InputText(
+            value = viewModel.password.value,
+            onValueChange = { viewModel.password.value = it },
+            text = "Contraseña",
+        )
+        InputText(
+            value = viewModel.fechaNacimiento.value,
+            onValueChange = { viewModel.fechaNacimiento.value = it },
+            text = "Fecha de Nacimiento",
+        )
+        InputText(
+            value = viewModel.telefono.value,
+            onValueChange = { viewModel.telefono.value = it },
+            text = "Telfono",
+        )
+        PrimaryButton("Aceptar",) {
+            viewModel.registro {
+                navController.navigate("menu") {
+                    popUpTo("registro") { inclusive = true }
+                }
+            }
         }
+
     }
-}
+    }
+
+private fun RegistroViewModel.registro(function: () -> Unit) {}
 
 @SuppressLint("ViewModelConstructorInComposable")
 @Preview(showBackground = true)
--- app/src/main/java/com/example/dely/ui/viewmodel/RegistroViewModel.kt
+++ app/src/main/java/com/example/dely/ui/viewmodel/RegistroViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.dely.ui.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 
-class RegistroViewModel: ViewModel(){
-
+class RegistroViewModel : ViewModel() {
+        var username = mutableStateOf("")
+        var email = mutableStateOf("")
+        var password = mutableStateOf("")
+        var fechaNacimiento = mutableStateOf("")
+        var telefono = mutableStateOf("")
 }
