0ad3adcbada8e6b2664d8700387076a81fed6b09
ChagoyanMau <20243ds137@utez.edu.mx>
2025-10-26 21:58:08 -0600

    Prototipo

---
+++ app/src/main/java/com/example/dely/ui/cards/ProductoList.kt
@@ -0,0 +1,48 @@
+package com.example.dely.ui.cards
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.Person
+import com.example.dely.ui.cards.Productos
+import com.example.dely.ui.cards.ProductosCard
+import com.example.dely.R
+import com.example.dely.ui.theme.DelyTheme
+
+@Composable
+fun ProductosList(lista: List<Productos>, x: (Productos) -> Unit) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        items(items = lista, key = {it.id} ) {producto ->
+            ProductosCard(producto) { x(producto) }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewProductosList(){
+    val lista = listOf(
+        Productos(
+            1,
+            "Pizza",
+            "Pizza Italiana",
+            "$250",
+            R.drawable.icono
+        ),
+        Productos(
+            2,
+            "Papas",
+            "Papas ala Francesa",
+            "60",
+            R.drawable.icono
+        ),
+    )
+    DelyTheme {
+        ProductosList(lista){}}
+}
--- app/src/main/java/com/example/dely/ui/viewmodel/ProductosListViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.dely.ui.viewmodel
-
-class ProductosListViewModel {
-}
